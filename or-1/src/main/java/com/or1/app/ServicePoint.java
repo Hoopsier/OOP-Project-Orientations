140b416bfe858f3df632adad181334d3968fb534
Renan <renanhoruz@gmail.com>
2026-03-27 13:45:43 +0200

    feat: 6

--- or-1/src/main/java/com/or1/app/ServicePoint.java
+++ or-1/src/main/java/com/or1/app/ServicePoint.java
@@ -1,15 +1,20 @@
 package com.or1.app;
 
+import java.util.*;
+
 class ServicePoint {
   private Queue queue;
   private long start;
   private long end;
+
+  private List<Long> times = new ArrayList<>();
 
   ServicePoint(Queue queue) {
     this.queue = queue;
   }
 
   public void serve() {
+    long sum = 0;
     if (queue.isEmpty()) {
       System.out.println("customers is empty");
     }
@@ -23,7 +28,12 @@ class ServicePoint {
         System.err.println("InterruptedException at Queue:handleQueue()");
       }
       end = System.currentTimeMillis();
+      times.add(end - start);
       System.out.println("Served customer " + customer.id + " in " + (end - start) + " ms");
     }
+    for (Long time : times) {
+      sum += time;
+    }
+    System.out.println("Average time spent per customer was " + (sum / times.size()));
   }
 }
