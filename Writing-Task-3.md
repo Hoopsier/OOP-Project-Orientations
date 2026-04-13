2a5fad3d33ec339aa363e94bf37dc4b4145aeca5
Renan <renanhoruz@gmail.com>
2026-04-13 12:13:02 +0300

    or-3 task 1&2

---
+++ Writing-Task-3.md
@@ -0,0 +1,8 @@
+# Orientation 3
+
+## 1
+
+I would create a static list of ages for the Student class, adding ages randomly,
+using a custom distribution.
+If there is a need to shove it into a graph,
+I'll print them to a `data.csv` file with `,` as separator.
---
+++ or-3/src/main/java/app/App.java
@@ -0,0 +1,32 @@
+package app;
+
+/**
+ * Hello world!
+ */
+public class App {
+  public static void main(String[] args) {
+    final int ageDistribution[][] = {
+        { 10, 19 }, { 50, 20 }, { 70, 21 }, { 80, 22 }, { 85, 23 }, { 90, 24 }, { 95, 25 }, { 100, 26 }
+    };
+    int size = ageDistribution.length;
+    /// starts at 19 ends at 26
+    int generatedAges[] = new int[size];
+
+    for (int i = 0; i < 1000; i++) {
+      int x = (int) (Math.random() * 100) + 1;
+      int j = 0;
+      while (x > ageDistribution[j][0]) {
+        j++;
+      }
+      generatedAges[j]++;
+    }
+    // Output the result of the generation:
+    System.out.println("Age  count     %-share");
+    for (int age = 0; age < size; age++) {
+      if (generatedAges[age] != 0) {
+        String str = "%-4d %-8d %-8.2f\n";
+        System.out.printf(str, age + 19, generatedAges[age], ((double) generatedAges[age]) / 1000 * 100);
+      }
+    }
+  }
+}
